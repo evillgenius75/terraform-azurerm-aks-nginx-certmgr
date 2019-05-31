51e40e341bc2c6f787debce57417f0425533dba2
Eddie Villalba <eddie.villalba@microsoft.com>
2019-05-31 08:40:26 -0500

    update kube config path

--- main_aks.tf
+++ main_aks.tf
@@ -73,7 +73,7 @@ resource "azurerm_kubernetes_cluster" "main" {
 # kube config and helm init
 resource "local_file" "kube_config" {
   # kube config
-  filename = "${var.K8S_KUBE_CONFIG}"
+  filename = "${var.K8S_KUBE_CONFIG}/${var.PROJECT}${var.INSTANCE}${var.ENVIRONMENT}${random_integer.uuid.result}aks"
   content  = "${azurerm_kubernetes_cluster.main.kube_config_raw}"
 
   # helm init
